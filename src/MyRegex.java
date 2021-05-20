4de8aec5df6b0733049473d6ed61b2d4933a7679
Witold Zolnowski <blue3656@gmail.com>
2021-05-20 21:25:45 +0100

    fix regex

--- src/MyRegex.java
+++ src/MyRegex.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-class Solution {
+class Solution{
 
-  public static void main(String[] args) {
+  public static void main(String[] args){
     Scanner in = new Scanner(System.in);
-    while (in.hasNext()) {
+    while(in.hasNext()){
       String IP = in.next();
       System.out.println(IP.matches(new MyRegex().pattern));
     }
+
   }
 }
-
 class MyRegex {
-  String pattern = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}";
+  //0-99 \\d{1,2}
+  //099 - 199 (0|1)\\{2}
+  //249 2[0-4]\\d
+  //255 25[0-5]
+  String pattern = "((\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])(\\.|$)){4}";
 }
