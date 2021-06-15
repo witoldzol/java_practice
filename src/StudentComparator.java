2408438df6fadd672ce7f23571c22a568fb28f65
Witold Zolnowski <blue3656@gmail.com>
2021-06-15 18:41:52 +0100

    use streams to sort

--- src/StudentComparator.java
+++ src/StudentComparator.java
@@ -1,51 +1,41 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
-class Student{
+class Student {
   private int id;
   private String fname;
   private double cgpa;
+
   public Student(int id, String fname, double cgpa) {
     super();
     this.id = id;
     this.fname = fname;
     this.cgpa = cgpa;
   }
+
   public int getId() {
     return id;
   }
+
   public String getFname() {
     return fname;
   }
+
   public double getCgpa() {
     return cgpa;
   }
 }
 
-class StudentComparator implements Comparator<Student>{
-  @Override
-  public int compare(Student s1, Student s2){
-    String s1Name = s1.getFname();
-    String s2Name = s2.getFname();
-    if(s1.getCgpa() == s2.getCgpa()){
-      if(s1Name.compareTo(s2Name) == 0){
-        return s1.getId() - s2.getId();
-      }
-      return s1Name.compareTo(s2Name);
-    }
-    double result = s2.getCgpa()*1000 - s1.getCgpa()*1000;
-    return (int) result;
-  }
-}
-
-public class Solution
-{
-  public static void main(String[] args){
+public class Solution {
+  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int testCases = Integer.parseInt(in.nextLine());
 
     List<Student> studentList = new ArrayList<Student>();
 
-    while(testCases>0){
+    while (testCases > 0) {
       int id = in.nextInt();
       String fname = in.next();
       double cgpa = in.nextDouble();
@@ -55,10 +45,9 @@ public class Solution
 
       testCases--;
     }
-    StudentComparator comparator = new StudentComparator();
-    Collections.sort(studentList, comparator);
+    studentList.sort(Comparator.comparing(Student::getCgpa).reversed().thenComparing(Student::getFname).thenComparing(Student::getId));
 
-    for(Student st: studentList){
+    for (Student st : studentList) {
       System.out.println(st.getFname());
     }
   }
