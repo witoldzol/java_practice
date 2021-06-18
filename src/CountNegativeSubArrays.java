6e16aead7b3c1a3fa7b6b3acbb4ba11bbd6fbabe
Witold Zolnowski <blue3656@gmail.com>
2021-06-18 07:24:24 +0100

    working solution n^3 complexity

--- src/CountNegativeSubArrays.java
+++ src/CountNegativeSubArrays.java
@@ -1,8 +1,10 @@
 import java.io.*;
 import java.util.*;
-
-public class Solution {
-
+public class CountNegativeSubArrays {
+  // three nested loops
+  // first defines starting point
+  // second defines end point ( inclusive )
+  // last one iterates from start to end point & sums up values
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int arraySize = scanner.nextInt();
@@ -10,33 +12,18 @@ public class Solution {
     for(int i=0;i<arraySize;i++){
       arr[i]=scanner.nextInt();
     }
-
-    int maxSizeOfSubArray = (int) arraySize/2;
-    List<Integer> subArraySizes = new ArrayList<>();
-    // create list of valid sub array sizes
-    subArraySizes.add(1);
-    for(int i=2; i<=maxSizeOfSubArray; i++){
-      if(arr.length%i == 0) subArraySizes.add(i);
-    }
-    subArraySizes.add(arr.length);
-
-    int negativeSubArrays = 0;
-    subArraySizes.forEach(subArrSize -> {
-      System.out.println(subArrSize);
-    });
-    //System.out.println(negativeSubArrays);
-  }
-
-  private static int countNegativeSubArrays(int[] arr, int subArraySize){
-    int negativeCount = 0;
-    //[1,2, 3,4,  5,6]
-    for(int i=0;i<arr.length - subArraySize;i+=subArraySize){
-      int sum = 0;
-      for(int k=0;k<subArraySize;k++){
-        sum += arr[i+k];
+    int negativeSubarray = 0;
+    //start
+    for(int i=0; i<arr.length; i++){
+      //end
+      for(int k=i; k<arr.length;k++){
+        int sum = 0;
+        for(int j=i; j<=k;j++){
+          sum += arr[j];
+        }
+        if (sum < 0) negativeSubarray++;
       }
-      if(sum<0) negativeCount++;
     }
-    return negativeCount;
+    System.out.println(negativeSubarray);
   }
 }
