0e5e1f14ee8d1f6e3cf1c7499afdf4a82a58ed02
Witold Zolnowski <blue3656@gmail.com>
2021-06-18 07:31:19 +0100

    improved solution n^2 complexity

--- src/CountNegativeSubArrays.java
+++ src/CountNegativeSubArrays.java
@@ -1,29 +1,22 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+
 public class CountNegativeSubArrays {
-  // three nested loops
-  // first defines starting point
-  // second defines end point ( inclusive )
-  // last one iterates from start to end point & sums up values
+
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int arraySize = scanner.nextInt();
     int[] arr = new int[arraySize];
-    for(int i=0;i<arraySize;i++){
-      arr[i]=scanner.nextInt();
+    for (int i = 0; i < arraySize; i++) {
+      arr[i] = scanner.nextInt();
     }
-    int negativeSubarray = 0;
-    //start
-    for(int i=0; i<arr.length; i++){
-      //end
-      for(int k=i; k<arr.length;k++){
-        int sum = 0;
-        for(int j=i; j<=k;j++){
-          sum += arr[j];
-        }
-        if (sum < 0) negativeSubarray++;
+    int count = 0;
+    for (int start = 0; start < arr.length; start++) {
+      int sum = 0;
+      for (int end = start; end < arr.length; end++) {
+        sum += arr[end];
+        if (sum < 0) count++;
       }
     }
-    System.out.println(negativeSubarray);
+    System.out.println(count);
   }
 }
