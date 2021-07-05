86e19d2b6e099f3d69dfaa866a6847f8e60339f4
Witold Zolnowski <blue3656@gmail.com>
2021-07-05 21:18:50 +0100

    phone book

---
+++ src/PhoneBook.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PhoneBook {
+  public static void main(String[] argh) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    in.nextLine();
+    Map<String, Integer> map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      String name = in.nextLine();
+      int phone = in.nextInt();
+      map.put(name, phone);
+      in.nextLine();
+    }
+    while (in.hasNext()) {
+      String s = in.nextLine();
+      if (map.containsKey(s)) {
+        System.out.println(s + "=" + map.get(s));
+      } else {
+        System.out.println("Not found");
+      }
+    }
+  }
+}
