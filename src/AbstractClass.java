e93c05791eb6ace98c43192b74841b1a9201483c
Witold Zolnowski <blue3656@gmail.com>
2021-07-10 14:57:53 +0100

    basic abstract class

---
+++ src/AbstractClass.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+abstract class Book {
+  String title;
+
+  abstract void setTitle(String s);
+
+  String getTitle() {
+    return title;
+  }
+}
+
+class MyBook extends Book {
+  void setTitle(String s) {
+    this.title = s;
+  }
+}
+
+public class AbstractClass {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String title = sc.nextLine();
+    MyBook new_novel = new MyBook();
+    new_novel.setTitle(title);
+    System.out.println("The title is: " + new_novel.getTitle());
+    sc.close();
+
+  }
+}
