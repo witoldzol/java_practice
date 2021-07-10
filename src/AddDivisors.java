5426e48c1a473fe537357d6ad66937aa063e86cf
Witold Zolnowski <blue3656@gmail.com>
2021-07-10 15:17:25 +0100

    stream sum of divisiors

--- src/AddDivisors.java
+++ src/AddDivisors.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.stream.IntStream;
 
 public class AddDivisors {
+  public static void main(String[] args) {
+    int n = 116;
+    int result = IntStream.rangeClosed(1,n/2).filter(i->n%i==0).sum()+n;
+    System.out.println(result);
+  }
 }
