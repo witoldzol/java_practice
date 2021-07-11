ce7def72f4be49a092d5f3fd793d995fe7f38c29
Witold Zolnowski <blue3656@gmail.com>
2021-07-11 18:48:23 +0100

    simple loop

---
+++ src/SimpleLoop.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Add {
+  void add(int... args) {
+    int sum = 0;
+    for (int i = 0; i < args.length - 1; i++) {
+      sum += args[i];
+      System.out.print(args[i] + "+");
+    }
+    sum += args[args.length - 1];
+    System.out.print(args[args.length - 1] + "=" + sum);
+    System.out.println("");
+  }
+}
+
+public class SimpleLoop {
+
+  public static void main(String[] args) {
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n1 = Integer.parseInt(br.readLine());
+      int n2 = Integer.parseInt(br.readLine());
+      int n3 = Integer.parseInt(br.readLine());
+      int n4 = Integer.parseInt(br.readLine());
+      int n5 = Integer.parseInt(br.readLine());
+      int n6 = Integer.parseInt(br.readLine());
+      Add ob = new Add();
+      ob.add(n1, n2);
+      ob.add(n1, n2, n3);
+      ob.add(n1, n2, n3, n4, n5);
+      ob.add(n1, n2, n3, n4, n5, n6);
+      Method[] methods = Add.class.getDeclaredMethods();
+      Set<String> set = new HashSet<>();
+      boolean overload = false;
+      for (int i = 0; i < methods.length; i++) {
+        if (set.contains(methods[i].getName())) {
+          overload = true;
+          break;
+        }
+        set.add(methods[i].getName());
+
+      }
+      if (overload) {
+        throw new Exception("Overloading not allowed");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
+
