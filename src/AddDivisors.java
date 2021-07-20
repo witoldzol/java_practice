36596f0fa3fd9abb95305cf1c31f6355d3048cc4
Witold <blue3656@gmail.com>
2021-07-20 22:04:27 +0100

    Comment

--- src/AddDivisors.java
+++ src/AddDivisors.java
@@ -3,6 +3,7 @@ import java.util.stream.IntStream;
 public class AddDivisors {
   public static void main(String[] args) {
     int n = 116;
+    // range closed is inclusive at the end
     int result = IntStream.rangeClosed(1,n/2).filter(i->n%i==0).sum()+n;
     System.out.println(result);
   }
